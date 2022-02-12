030ebd0399f64117417a66efebacabba27bfdf3f
Johnny Rubi <johnnyrubischreiner@gmail.com>
2022-02-11 22:47:04 -0300

    add: service concluido

---
+++ src/service/index.js
@@ -0,0 +1,9 @@
+const validationPages = require("../utils/validation");
+const pagination = require("../logic");
+
+const serviceController = ({page, pageSize}) => {
+    validationPages(page, pageSize);
+    return pagination(Number(page), Number(pageSize));
+};
+
+module.exports = serviceController;
