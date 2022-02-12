5d53909d1a4c01022539c833dec21d27f6f1c07e
Johnny Rubi <johnnyrubischreiner@gmail.com>
2022-02-11 22:48:08 -0300

    add: logica para paginaçao concluida

---
+++ src/logic/index.js
@@ -0,0 +1,70 @@
+const pageSizeLessThanFive = (page, pageSize) => {
+    const array = [];
+
+    for (let i = 1; i <= pageSize; i++) {
+        if (page === i) {
+            array.push(`**${page}**`);
+        } 
+        else {
+            array.push(`${i}`);
+        }
+    };
+    return array;
+};
+
+const treePointsEnd = (page) => {
+    const array = [];
+
+    for (let i = 1; i <= 5; i++) {
+        if (page === i) {
+            array.push(`**${page}**`);
+        }
+        else {
+            array.push(`${i}`);
+        }
+    }
+    array.push('...');
+    return array;
+};
+
+const finalPages = (page, pageSize) => {
+    const array = ['...'];
+
+    for (let i = pageSize - 4; i <= pageSize; i++) {
+        if (page === i) {
+            array.push(`**${page}**`);
+        } 
+        else {
+            array.push(`${i}`);
+        }
+    };
+    return array;
+};
+
+const treePointsEndandInit = (page) => {
+    const array = ['...'];
+
+    for (let i = page - 2; i <= page + 2; i++) {
+        if (page === i) {
+            array.push(`**${page}**`);
+        } 
+        else {
+            array.push(`${i}`);
+        }
+    };
+    array.push('...');
+    return array;
+};
+
+
+const pagination = (page, pageSize) => {
+    if (pageSize <= 5) return pageSizeLessThanFive(page, pageSize);
+
+    if (page < 4 && pageSize > 5) return treePointsEnd(page);
+
+    if (page > (pageSize - 3)) return finalPages(page, pageSize);
+
+    return treePointsEndandInit(page);
+};
+
+module.exports = pagination
